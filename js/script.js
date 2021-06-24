a3116886495c64269a879ab5244fd3fefa948276
unknown <marcelofariam23@outlook.com>
2021-06-24 17:56:59 -0300

    Formulario

--- js/script.js
+++ js/script.js
@@ -1,19 +1,63 @@
-const checkInputs = document.querySelectorAll('input[type="checkbox"]');
-const btnSend = document.querySelector('.btn-send');
-
 const verifyCheckInputs = () => {
-    for(let i=0; i <= checkInputs.length; i++){
+    const checkInputs = document.querySelectorAll('input[type="checkbox"]');
+    for (let i=0; i <= checkInputs.length; i++){
+        
         let checked = document.querySelectorAll('input[type="checkbox"]:checked').length;
-
-        if(checked == 0) {
-            console.log('erro')
+        if (checked == 0) {
+            alert('Selecione ao menos um stick');
             return false;
         }
-        else {
-            console.log(checked)
-            return true;
-        }
+        return true
     }
 }
 
-btnSend.addEventListener('click', () => verifyCheckInputs());
+const btnIncrement = document.querySelector('.btn-plus');
+const btnDecrement = document.querySelector('.btn-minus');
+const quantity = document.querySelector('.quantity');
+
+btnIncrement.addEventListener('click', () => {
+    quantity.value++;
+})
+
+btnDecrement.addEventListener('click', () => {
+    quantity.value--;
+
+    if (quantity.value < 0) {
+        quantity.value = 0;
+    }
+})
+
+const verifyEmptyFields = () => {
+    if (quantity.value == 0) {
+        quantity.classList.add('error');
+        return false;
+    } 
+    else {
+        quantity.classList.remove('error');
+        return true;
+    }
+}
+
+const clearAllFields = () => {
+    const checkbox = document.querySelectorAll('.form-check-input'); 
+    const textArea = document.querySelector('.text-area');   
+    for (let i of checkbox) {
+        if (i.checked) {
+            i.checked = !i.checked;
+        }
+    }
+    quantity.value = 0;
+    textArea.value = '';
+}
+
+// Button "Enviar"
+const btnSend = document.querySelector('.btn-send');
+
+// Message
+const message = document.querySelector('.message');
+btnSend.addEventListener('click', () => {
+    if (verifyCheckInputs() && verifyEmptyFields()) {
+        message.classList.add('visible');
+        clearAllFields();
+    }
+});
